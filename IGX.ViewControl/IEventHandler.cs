19bf045a0ae8b89ad91c9ae2610327c62939e7ad
kyum0627 <120994749+kyum0627@users.noreply.github.com>
2025-12-10 18:28:29 +0900

    Delete IGX.ViewControl directory

--- IGX.ViewControl/IEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenTK.GLControl;
-
-namespace IGX.ViewControl
-{
-    public interface IEventHandler
-    {
-        void SetupEvents(GLControl control, Action render, Action<int, int> selectObject, Action setupViewport);
-    }
-}
